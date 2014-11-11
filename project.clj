a5ed240e234c076ac564a84d11dbff3982a8d050
Yohan Pereira <yohan.pereira@gmail.com>
2014-11-11 13:29:05 +0530

    update version and add url.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject browserstack-clj "0.1.3-SNAPSHOT"
+(defproject browserstack-clj "0.1.4-SNAPSHOT"
   :description "A Clojure wrapper over the browserstack API."
+  :url         "https://github.com/yohan-pereira/browserstack-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
